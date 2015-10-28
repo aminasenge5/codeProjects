5871198df17441faa15fe3603604de7d791cdc11
Adin <buddhamensch@yahoo.com>
2015-10-28 14:53:20 -0700

    This is the October 28th update to the GuessingGame index.html  and game.js files

--- GuessingGame/game.js
+++ GuessingGame/game.js
@@ -1,9 +1,13 @@
 // establish that 'i' will change as questions are guessed
 var i = 0;
 
+var name = prompt("Welcome! What's your name?");
+
+alert("Welcome " + name + "! Let's play!");
+
 // asking questions
 var answer1 = prompt("Does Adin come from San Francisco?");
-if (answer1 === "YES") {
+if (answer1 === "YES" || answer1 === "yes" || answer1 === "y" || answer1 === "Yes") {
 	i++;
 	alert("You're right!");
 } else {
@@ -11,7 +15,7 @@ if (answer1 === "YES") {
 }
 console.log(answer1);
 var answer2 = prompt("Does Adin speak Russian?"); 
-if (answer2 === "NO") {
+if (answer2 === "NO" || answer2 === "no" || answer2 === "n" || answer2 === "No") {
 	i++;
 	alert("You're right!");
 } else {
@@ -19,10 +23,11 @@ if (answer2 === "NO") {
 }
 console.log(answer2);
 var answer3 = prompt("Adin was a high school teacher before moving to PDX?");
-if (answer3 === "YES") {
+if (answer3 === "YES" || answer3 === "yes" || answer3 === "y" || answer3 === "Yes") {
+	i++;
 	alert("You're right!");
 } else {
 	alert("Sorry you're wrong. Adin WAS a high school teacher.");
 }
 console.log(answer3);
-alert("You guessed " + i + " right!");
+alert("You guessed " + i + " right! Thank you for playing, " + name + "!");
